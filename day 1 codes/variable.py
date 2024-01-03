59020869dbb0ba7ac355bf653cc3109b892c6c79
Soumili Lahiri <155342372+Mili-sou@users.noreply.github.com>
2024-01-03 09:45:54 +0530

    variable.py

---
+++ day 1 codes/variable.py
@@ -0,0 +1,12 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+t=a
+a=b
+b=t
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
